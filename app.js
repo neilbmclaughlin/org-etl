98754c3da3fa4190681559f7c4f1e4326ad2a01b
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2017-01-12 10:59:58 +0000

    --amend

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const fs = require('fs');
 const parse = require('csv-parse');
 const transform = require('stream-transform');
 const request = require('request');
-var options = { db: 'mongodb://localhost:27017/local', collection: 'services' }
-var streamToMongo = require('stream-to-mongo')(options);
+const streamToMongoDB = require("stream-to-mongo-db").streamToMongoDB;
+
+const outputDBConfig = { dbURL : "mongodb://localhost:27017/local", collection : "services" };
+
+const collectionWriteStream = streamToMongoDB(outputDBConfig);
 
 urlGenerator = function(record, callback){
       const odsCode = record[0];
@@ -14,41 +17,35 @@ urlGenerator = function(record, callback){
       if (active === 'A' && orgSubType === '1') {
         url = `https://api.nhs.uk/organisations/${odsCode}`;
         // console.log(url);
-        callback(null, url + '\n');
+        callback(null, url);
       }
 }
 
 orgDetailsRetriever = function (url, callback) {
-		request(url, (err, res, body) => {
-			if (err) {
-        console.log(`Download of ${url} returned ${res.statusCode}`);
-			} else {
-				switch (res.statusCode) {
-					case 200:
-            callback(null, JSON.parse(body.replace('\"identifier\"', '\"_id\"')));
-            // callback(null, body.replace('\"identifier\"', '\"_id\"'));
-						break;
-					case 404:
-            console.log(`404 ${url}`);
-						break;
-					case 500:
-            console.log(`500 ${url}`);
-						break;
-					default:
-						console.log(`Download of ${url} returned ${res.statusCode}`);
-				}
+	request(url, (err, res, body) => {
+		if (err) {
+			console.log(`Download of ${url} errored (${err.message})`);
+		} else {
+			switch (res.statusCode) {
+				case 200:
+					callback(null, JSON.parse(body.replace('\"identifier\"', '\"_id\"')));
+					// callback(null, body.replace('\"identifier\"', '\"_id\"'));
+					break;
+				default:
+					console.log(`Download of ${url} returned status code ${res.statusCode || 'unknown'}`);
 			}
-		});
-
-
+		}
+	});
 }
 
 const input = fs.createReadStream('./edispensary-50.csv');
-
 const parser = parse({delimiter: ','})
 const urlTransformer = transform(urlGenerator);
 const orgTransformer = transform(orgDetailsRetriever);
 
-// input.pipe(parser).pipe(urlTransformer).pipe(orgTransformer).pipe(fs.createWriteStream('org-list.json'));
-input.pipe(parser).pipe(urlTransformer).pipe(orgTransformer).pipe(streamToMongo);
+input.pipe(parser)
+    .pipe(urlTransformer)
+    .pipe(orgTransformer)
+    // .pipe(fs.createWriteStream('org-list.json'));
+    .pipe(collectionWriteStream);
 
